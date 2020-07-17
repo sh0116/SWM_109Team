d4c3b0f3479274abb0fce3c2b1fa19452876c755
hyunjigonji <guswl5404@hanyang.ac.kr>
2020-07-17 15:11:16 +0900

    detecting falldown with yolo
    Former-commit-id: e220a0e293261a65edfe6d5ad15d8743c11a2ee6

---
+++ falldown/yolo_object_detection.py
@@ -0,0 +1,90 @@
+import cv2
+import numpy as np
+import time 
+
+# Load Yolo
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+blue_color = (255,0,0)
+red_color = (0,0,255)
+
+# Open Cam
+try:
+    print("open cam")
+    cap = cv2.VideoCapture(0)
+except:
+    print("not working")
+cap.set(3, 600)
+cap.set(4, 600)
+
+origin_time = 0
+# Taking video
+while True:
+    # Loading image
+    ret, frame = cap.read()
+    #frame = cv2.resize(frame, (600,600), cv2.INTER_AREA)
+    height, width, channels = frame.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            if str(classes[class_id]) != 'person': continue
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            if w > h:
+                #print("fall down!!")
+                color = red_color
+                if origin_time == 0:
+                    origin_time = time.time()
+                else:
+                    now_time = time.time()
+                    if now_time - origin_time >= 5:
+                        print("fall down!")
+                        origin_time = 0
+            else:
+                color = blue_color
+            #label = str(classes[class_ids[i]])
+            #color = colors[i]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            #cv2.putText(frame, label, (x, y + 30), font, 3, color, 3)
+    cv2.imshow("Frame", frame)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27: break
+    #time.sleep(1)
+cv2.destroyAllWindows()
