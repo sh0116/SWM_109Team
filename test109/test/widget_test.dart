6bc9bacc291b5aae3fc02c400fcdae0014dd6a6c
hyunjigonji <guswl5404@hanyang.ac.kr>
2020-07-29 20:15:36 +0900

    register page in flutter
    Former-commit-id: a61f74619441656b17fe58f01ede880b7aa53416

--- test109/test/widget_test.dart
+++ test109/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test109/main.dart';
+import 'package:test109/userInfo.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
