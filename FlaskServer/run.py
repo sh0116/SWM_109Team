e78713dc885e4ef5d2760a856fde66810b571255
danbicyy <danbicyy@naver.com>
2020-08-11 05:25:44 +0900

    Web map modify

--- FlaskServer/run.py
+++ FlaskServer/run.py
@@ -3,8 +3,7 @@ from flask import render_template
 from flask_restful import Resource, Api
 from flask_bootstrap import Bootstrap
 #from flask_mysqldb import MySQL
-
-
+import dbapi
 app = Flask(__name__)
 api = Api(app)
 Bootstrap(app)
@@ -16,7 +15,29 @@ class RegistUser(Resource):
 api.add_resource(RegistUser,'/user')
 @app.route('/')
 def index():
-    return render_template('index.html')
+    row = dbapi.select_fall_down()
+    data = dbapi.select_body_temp()
+    data1 = dbapi.select_user_info()
+    data2 = dbapi.select_addresstest()
+    return render_template('index.html',row=row, data = data, data1 = data1, data2 = data2)
+
+@app.route('/weekly')
+def index1():
+    row = dbapi.select_fall_down()
+    data = dbapi.select_body_temp()
+    data1 = dbapi.select_user_info()
+    return render_template('index1.html',row=row, data = data, data1 = data1)
+
+
+@app.route('/monthly')
+def index2():
+    return render_template('index2.html')
+
+@app.route('/contact')
+def index3():
+    return render_template('Contacts.html')
+
+
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=5000, debug=True)
