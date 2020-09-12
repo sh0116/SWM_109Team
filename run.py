e1fe08faa0657ab1e1040f214fb549644128d7f3
danbicyy <danbicyy@naver.com>
2020-09-13 03:25:26 +0900

    modify data stream trigger
    Former-commit-id: 2a835f29a82b1b1e224cbf9a77780ba5b80bd976

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
+# -*- coding:utf-8 -*-
 from flask import Flask
 from flask import render_template
 from flask_restful import Resource, Api
 from flask_bootstrap import Bootstrap
 from flask import  request, Response
+from flask import stream_with_context,flash                                                                                                                         
+from time import sleep
 #from flask_mysqldb import MySQL
 import dbAPI
 import sys
 reload(sys)
 sys.setdefaultencoding("UTF-8")
 app = Flask(__name__)
+app.config['JSON_AS_ASCII']=False
 api = Api(app)
 Bootstrap(app)
 
@@ -43,19 +47,19 @@ def user_info_post(data):
     if request.method == 'GET': # SELECT    
         prot_id = request.args['prot_id']
         if(data == 'id'):
-            user = dbAPI.select_where("user_info",1,'id',prot_id=prot_id)
+            user = dbAPI.select_where("user_info",0,'id',prot_id=prot_id)
         elif(data == 'name'):
-            user = dbAPI.select_where("user_info",1,"name",prot_id=prot_id)
+            user = dbAPI.select_where("user_info",0,"name",prot_id=prot_id)
         elif(data == 'gender'):
-            user = dbAPI.select_where("user_info",1,"gender",prot_id=prot_id)
+            user = dbAPI.select_where("user_info",0,"gender",prot_id=prot_id)
         elif(data == 'birth'):
-            user = dbAPI.select_where("user_info",1,"birth",prot_id=prot_id)
+            user = dbAPI.select_where("user_info",0,"birth",prot_id=prot_id)
         elif(data == 'address'):
-            user = dbAPI.select_where("user_info",1,"address",prot_id=prot_id)
+            user = dbAPI.select_where("user_info",0,"address",prot_id=prot_id)
         elif(data == 'contact'):
-            user = dbAPI.select_where("user_info",1,"contact",prot_id=prot_id)
+            user = dbAPI.select_where("user_info",0,"contact",prot_id=prot_id)
         #print(user)
-        return str(user[0])
+        return str(user)
     else: # POST (INSERT)
         name = request.get_json().get('name')
         gender = request.get_json().get('gender')
@@ -90,6 +94,77 @@ def prot_info_post(data):
         dbAPI.insert_prot_info(name, contact)
         return "prot_info_post"
 
+@app.route('/userapp')
+def Userapp():
+    temp = request.args.get('prot_id')
+    temp1 = request.args.get('user_id')
+    graph_fall = dbAPI.select_fall_down(user_id = int(temp1))
+    count_fall = dbAPI.select_fall_down_count(user_id = int(temp1))
+    wake_up = dbAPI.select_wake_up(user_id = int(temp1))
+    sleep = dbAPI.select_sleep(user_id = int(temp1))
+    temperature = dbAPI.select_where("sensor_data",0,"num",sensor_id = 1, user_id = int(temp1))
+    humidity = dbAPI.select_where("sensor_data",0,"num",sensor_id = 2, user_id = int(temp1))
+    user_info = dbAPI.select_where("user_info",0,"*",id=int(temp1))
+    prot_info = dbAPI.select_where("prot_info",0,"*",id=int(temp))
+    
+    return render_template('userapp.html',row = graph_fall, data = temperature, data1 = user_info, data2 = prot_info,row1 = count_fall, data3 = humidity,sleep = wake_up)
+
+
+#temperature(1), humidity(2), wake_up(3), sleep(4), fall_down(5), activity(6) 
+@app.route('/')
+def index():
+    graph_fall = dbAPI.select_fall_down(user_id = 2)
+    count_fall = dbAPI.select_fall_down_count(user_id = 1)
+    wake_up = dbAPI.select_wake_up(user_id = 1)
+    sleep = dbAPI.select_sleep(user_id = 1)
+    temperature = dbAPI.select_where("sensor_data",0,"num",sensor_id = 1, user_id = 1)
+    humidity = dbAPI.select_where("sensor_data",0,"num",sensor_id = 2, user_id = 1)
+    user_info = dbAPI.select_where("user_info",0,"*",id=1)
+    all_user_info = dbAPI.select("user_info",0, "*")
+    
+    return render_template('index.html',row = graph_fall, data = temperature, data1 = user_info, data2 = all_user_info ,row1 = count_fall, data3 = humidity,sleep = wake_up)
+
+@app.route('/contact')
+def Contact():
+    user_info = dbAPI.select_where("user_info",0,"*",id=1)
+    temperature = dbAPI.select_where("sensor_data",0,"num",sensor_id = 1, user_id = 1)
+    return render_template('Contacts.html',row = user_info)
+@app.route('/ajax',methods = ["GET",'POST'])
+def query():
+    target_user = request.form.get('student_id')
+    user_info = list()
+    sensor_data = list()
+    for i in list(dbapi.get_target_data2db( "user_info", target_user ))[0]:
+        user_info.append(i)
+    #print(list(dbapi.get_target_data2db( "humidity", target_user ))[0])
+    print(dbapi.select_humidity())
+    print(dbapi.select_fall_down())
+    print(dbapi.select_temp())
+    #print(list(dbapi.get_target_data2db( "temperature", target_user ))[0])
+    #print(list(dbapi.get_target_data2db( "fall_down", target_user ))[0])
+    #for i in list(dbapi.get_target_data2db( "humidity", target_user ))[0]:
+    #for i in list(dbapi.get_target_data2db( "temperature", target_user ))[0]:
+    #for i in list(dbapi.get_target_data2db( "fall_down", target_user ))[0]:
+
+    return str(user_info)
+
+# def stream_template(template_name, **context):                                                                                                                                                 
+#     app.update_template_context(context)                                                                                                                                                       
+#     t = app.jinja_env.get_template(template_name)                                                                                                                                              
+#     rv = t.stream(context)                                                                                                                                                                     
+#     rv.disable_buffering()                                                                                                                                                                     
+#     return rv
+
+# data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]                                                                                                                                                          
+# def generate():                                                                                                                                                                                
+#     for item in data:                                                                                                                                                                          
+#         yield str(item)                                                                                                                                                                        
+#         sleep(1)                                                                                                                                                                               
+
+# @app.route('/stream')                                                                                                                                                                          
+# def stream_view():                                                                                                                                                                             
+#     stream_ing = generate()                                                                                                                                                                          
+#     return Response(stream_template('index.html', stream_ing=stream_ing))
 
 @app.route('/ffff', methods = ['POST'])
 def ffff():
@@ -102,26 +177,9 @@ def ffff():
     else:
         return 'ffff'
 
-#temperature(1), humidity(2), wake_up(3), sleep(4), fall_down(5), activity(6)
-
-@app.route('/')
-def index():
-    graph_fall = dbAPI.select_fall_down()
-    count_fall = dbAPI.select_fall_down_count()
-    wake_up = dbAPI.select_wake_up()
-    sleep = dbAPI.select_sleep()
-    temperature = dbAPI.select_where("sensor_data",0,"num",sensor_id = 1, user_id = 1)
-    humidity = dbAPI.select_where("sensor_data",0,"num",sensor_id = 2, user_id = 1)
-    user_info = dbAPI.select_where("user_info",0,"*",id=1)
-    
-    return render_template('index.html',row = graph_fall, data = temperature, data1 = user_info, row1 = count_fall, data3 = humidity,sleep = wake_up)
-
-@app.route('/contact')
-def index3():
-    user_info = dbAPI.select_where("user_info",0,"*",id=1)
-    temperature = dbAPI.select_where("sensor_data",0,"num",sensor_id = 1, user_id = 1)
-    return render_template('Contacts.html',row = user_info)
-
+@app.route('/ajax-trigger') 
+def ajax_trigger(): 
+    return my_algorithm()
 
 
 if __name__=='__main__':
