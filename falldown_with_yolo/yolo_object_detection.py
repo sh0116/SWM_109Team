f315589a5e06faa3d5df2bfc44bda1d51a94fb49
이현지 <hyunjigonji@ihyeonjiui-MacBookPro.local>
2020-10-20 20:31:52 +0900

    modified function to calculate speed
    Former-commit-id: 42d0ab29502c53e8a87027e6a831f42807b62447

--- falldown_with_yolo/yolo_object_detection.py
+++ falldown_with_yolo/yolo_object_detection.py
@@ -3,11 +3,12 @@ import numpy as np
 import time
 import enum
 import requests
-import pygame
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+import math
 
-URL = 'http://13.125.221.213:5555/fall_down'
-data = {'user_id' : '1'}
-
+URL = 'http://13.125.221.213:5000/sensor'
+sched = BackgroundScheduler()
 # Load Yolo
 net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 classes = []
@@ -22,55 +23,82 @@ green_color = (0,255,0)
 try:
     print("open cam")
     cap = cv2.VideoCapture(0)
-    #cap = cv2.VideoCapture('/Users/hyunjigonji/yolo_object_detection')
+    #cap = cv2.VideoCapture('./example1.mp4')
 except:
     print("not working")
-cap.set(3, 800)
-cap.set(4, 600)
-
-def play_mp3_question():
-    pygame.mixer.init()
-    pygame.mixer.music.load("./question.mp3")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
-    #print("play over...")
-    return
-
-def play_mp3_notice():
-    pygame.mixer.init()
-    pygame.mixer.music.load("./notice.mp3")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
-    #print("play over...")
-    return
+#cap.set(3, 800)
+#cap.set(4, 600)
 
 class status(enum.Enum):
     standing = 0
     lying = 1
     falldown = 2
 
+
+origin_time = 0
+now_time = 0
 beforeStatus = status.standing
 nowStatus = status.standing
+beforeCenterX = 0
+beforeCenterY = 0
 
-standing_time = time.time()
-lying_time = time.time()
-falldown_time = time.time()
+global realtime
+standingMinX=0
+standingMaxX=0
+standingMinY=0
+standingMaxY=0
+fallMinX=0
+fallMaxX=0
+fallMinY=0
+fallMaxY=0
+flag = False
 
-hasPrinted = False
+beforeH = 1
+standingTime = time.time()
+
+userHeight = 160
+
+realtime=0
+
+def realtime_count(Cpx,Cpy,Bpx,Bpy):
+    global realtime
+    if abs(Cpx - Bpx) >=15 or abs(Cpy-Bpy) >=15:
+        realtime+=1
+
+def push_realtime():
+    global realtime
+    data = {'user_id' : '1', 'sendor_id':6, 'num' : realtime,'day': 'saturday'}
+    #res = requests.post(URL, json=data)
+    #print(realtime)
+    realtime = 0
+sched.add_job(push_realtime, 'interval', seconds=20)
+            
+sched.start()
 
 # Taking video
 while True:
     # Loading image
     ret, frame = cap.read()
+    #frame = cv2.resize(frame, (600,600), cv2.INTER_AREA)
     height, width, channels = frame.shape
+    #height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    #width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 
     # Detecting objects
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    #blob = cv2.dnn.blobFromImage(frame)
+
     net.setInput(blob)
     outs = net.forward(output_layers)
 
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    
+    # sched = BlockingScheduler()
+    # sched.add_job(push_realtime, 'interval', seconds=1)
+    # sched.start()
     for out in outs:
         for detection in out:
             scores = detection[5:]
@@ -78,56 +106,136 @@ while True:
             if str(classes[class_id]) != 'person': continue
             confidence = scores[class_id]
             if confidence > 0.8: # person detected when confidence is over 0.8
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
+                CenterPointX = int(detection[0] * width)
+                CenterPointY = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
                 # Rectangle coordinates
-                x = int(center_x - w/2)
-                y = int(center_y - h/2)
-                if w/2 > h: # fall down
-                    now_time = time.time()
-                    diff_time = now_time - standing_time
-                    if diff_time <= 1:
-                        falldown_time = time.time()
-                        nowStatus = status.falldown
-                        color = red_color
-                        print("fall down 111")
-                        play_mp3_question()
-                    elif beforeStatus == status.falldown:
-                        if diff_time >= 5 and diff_time < 10:
-                            falldown_time = time.time()
-                            nowStatus = status.falldown
-                            color = red_color
-                            if hasPrinted == False:
-                                print("fall down 222")
-                                play_mp3_question()
-                                hasPrinted2 = True
-                        elif diff_time >= 10:
-                            falldown_time = time.time()
-                            nowStatus = status.falldown
-                            color = red_color
-                            print("fall down 333")
-                            play_mp3_notice()
-                            quit()
-                    else: # lying
-                        lying_time = time.time()
-                        nowStatus = s.lying
-                        color = green_color
-                elif w > h: # lying
-                    lying_time = time.time()
+                x = int(CenterPointX - w/2)
+                y = int(CenterPointY - h/2)
+                
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    
+    for i in range(len(boxes)):
+        if i in indexes:
+            x,y,w,h = boxes[i]
+            HeadPointX = CenterPointX
+            HeadPointY = y
+            FootPointX = CenterPointX
+            FootPointY = h+y
+
+            if w/2 > h: # detected
+                color = red_color
+                if beforeStatus == status.standing or beforeStatus == status.falldown:
+                    nowStatus = status.falldown
+                    if origin_time == 0:
+                        origin_time = time.time() # count start
+                        #print("fall detected!")
+                        
+                        fallMinX = x
+                        fallMaxX = x+w
+                        fallMinY = y
+                        fallMaxY = y+h
+
+                        HeadPointX = fallMinX if abs(fallMaxX-standingMaxX) < abs(fallMinX-standingMinX) else fallMaxX
+                        HeadPointY = CenterPointY
+
+                        FootPointX = fallMinX if HeadPointX==fallMaxX else fallMaxX
+                        FootPointY = CenterPointY
+                    
+                        distance = math.sqrt( (beforeCenterX-HeadPointX)**2 + (standingMinY-HeadPointY)**2 )
+                        realDist = userHeight * distance / beforeH
+                        if (time.time() - standingTime) != 0:
+                            v = realDist / (time.time() - standingTime)
+                            if flag is False:
+                                print(str(nowStatus)[7:])
+                                print(v/100)
+                                #print("HP : {},{}".format(HeadPointX,HeadPointY))
+                                #print("CP : {},{}".format(CenterPointX,CenterPointY))
+                                flag = True
+                            
+                        #play_mp3_question()
+                    else:
+                        now_time = time.time()
+                        if now_time - origin_time >= 5 and now_time - origin_time < 10 and origin_time != 0:
+                            #print(args)
+                            print("fall down 22!")
+                            #play_mp3_question()
+                        elif now_time - origin_time >= 10 and origin_time != 0:
+                            #print(args)
+                            print("fall down 33!")
+                            #play_mp3_notice()
+                            #res = requests.post(URL, json=data)
+                            #origin_time = 0
+                            #exit()
+                else: # lying
                     nowStatus = status.lying
+                    origin_time = 0
                     color = green_color
-                else: # standing
-                    standing_time = time.time()
-                    nowStatus = status.standing
-                    color = blue_color
-                label = str(nowStatus)[7:] # parsing status to label
-                font = cv2.FONT_HERSHEY_SIMPLEX # define font style
-                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 10) # draw rectangle
-                cv2.putText(frame, label, (x, y - 30), font, 3, color, 7) # write text
-                beforeStatus = nowStatus
+                    
+                    fallMinX = x
+                    fallMaxX = x+w
+                    fallMinY = y
+                    fallMaxY = y+h
+
+                    HeadPointX = fallMinX if abs(fallMaxX-standingMaxX) < abs(fallMinX-standingMinX) else fallMaxX
+                    HeadPointY = CenterPointY
+
+                    FootPointX = fallMinX if HeadPointX==fallMaxX else fallMaxX
+                    FootPointY = CenterPointY
+                    distance = math.sqrt( (beforeCenterX-HeadPointX)**2 + (standingMinY-HeadPointY)**2 )
+                    realDist = userHeight * distance / beforeH
+                    if (time.time() - standingTime) != 0:
+                        v = realDist / (time.time() - standingTime)
+                        if flag is False:
+                            print(str(nowStatus)[7:])
+                            print(v/100)
+                            #print("HP : {},{}".format(HeadPointX,HeadPointY))
+                            #print("CP : {},{}".format(CenterPointX,CenterPointY))
+                            flag = True
+            elif w > h: # lying
+                nowStatus = status.lying
+                color = green_color
+                origin_time = 0
+            else: # standing
+                nowStatus = status.standing
+                color = blue_color
+                origin_time = 0
+                realtime_count(CenterPointX,CenterPointY,beforeCenterX,beforeCenterY)
+                beforeCenterX = CenterPointX
+                beforeCenterY = CenterPointY
+                standingMinX = x
+                standingMaxX = x+w
+                standingMinY = y
+                standingMaxY = y+h
+                beforeH = h
+                standingTime = time.time()
+                flag = False
+                
+            label = str(nowStatus)[7:]
+            font = cv2.FONT_HERSHEY_PLAIN
+            #color = colors[i]
+            hpstr = "HP : {},{}".format(HeadPointX,HeadPointY)
+            cpstr = "CP : {},{}".format(CenterPointX,CenterPointY)
+            #cv2.putText(frame, hpstr,(5,20), font, 2, red_color,2)
+            #cv2.putText(frame, cpstr,(5,60), font, 2, red_color,2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 10)
+            cv2.putText(frame, label, (x, y - 30), font, 7, color, 7)
+            cv2.circle(frame, (CenterPointX, CenterPointY), 10, (0,255,255), -1)
+            cv2.circle(frame, (HeadPointX, HeadPointY), 10, (0,0,255), -1)
+            cv2.line(frame,(HeadPointX, HeadPointY),(FootPointX, FootPointY),green_color,5)
+            #print(center_Pointw,center_Pointy)
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+            beforeStatus = nowStatus
     cv2.imshow("Frame", frame)
     k = cv2.waitKey(1) & 0xFF
     if k == 27: break
+    #time.sleep(0.5)
 cv2.destroyAllWindows()
