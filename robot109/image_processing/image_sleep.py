9544bd4e5704ef028100ef8d1d70b16382f67e94
danbicyy <danbicyy@naver.com>
2020-11-03 21:23:16 +0900

    modify image_activity,image_sleep error
    Former-commit-id: a7bba3b4d4c65ee30c3d2d1c1983fe1d7314d456 [formerly 1f3befa0e597d2e21c1f8b9409d998026782e8cc]
    Former-commit-id: 326b310a5809f762380dd95a8354445446d0ae65

--- robot109/image_processing/image_sleep.py
+++ robot109/image_processing/image_sleep.py
@@ -1,46 +1,40 @@
+import dataCenter
+import requests
 from datetime import datetime
 
-global day, cal_day, pre_time
-day = ['monday','tuesday','wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-cal_day = datetime.today().weekday()
+global pre_time,nowTuple,sleep_hour, sleep_min, seconds, sleep_time
 pre_time = datetime.now()
 nowTuple = pre_time.timetuple()
-
-global sleep_hour, sleep_min, seconds, sleep_time
 sleep_hour = int(nowTuple.tm_hour)
 sleep_min = int(nowTuple.tm_min)
 seconds = (pre_time.hour * 60 + pre_time.minute) * 60 + pre_time.second
 sleep_time = seconds/3600
 
-global sleep_check, wake_check
-sleep_check = 0
-wake_check = 0
 
-def day_sleep_time():
-    global day, cal_day, pre_time
-    global sleep_hour, sleep_min, seconds, sleep_time
-    
+def day_sleep_time(sleep_check):
+    global sleep_time,sleep_hour,sleep_min
     sleep_time = seconds/3600
-    #21pm~12:30am
+    print(sleep_hour, sleep_min)
+    #75600
     if (seconds >= 75600 and seconds <= 86339) or (seconds >= 0 and seconds <= 1800) :
         if sleep_check == 0:
             sleep_check +=1
-            data = {'user_id' : '1', 'graph' : sleep_time, 'day': day[cal_day]}
+            data = {'user_id' : dataCenter.user_id, 'sensor_id': dataCenter.sleep, 'num' : sleep_time, 'day': 'sunday'}
+            requests.post(dataCenter.URL, json=data)
+            return sleep_check
     else:
         sleep_check = 0
-    #print("sleep time : {}-{}-{} {}:{}:{}".format(pre_time.year, pre_time.month, pre_time.day,pre_time.hour,pre_time.minute,pre_time.second))
-    #print("sleep %d",sleep_time,day[cal_day])
 
-def day_wake_time():
-    global day, cal_day, pre_time, sleep_hour, sleep_min, seconds,sleep_check
-    sleep_time = seconds/3600
-    #4am ~ 7:30am
+def day_wake_time(wake_check,sleepingtime):
+    global sleep_time,sleep_hour,sleep_min
+    sleep_time = seconds/60
+    sleepingtime /= 3600 
     if seconds >= 14400 and seconds<=27000:
         if wake_check == 0:
             wake_check +=1
-            data = {'user_id' : '3', 'graph' : wake_up_time, 'day': day[cal_day]}
-            #print("wake up time : {}-{}-{} {}:{}:{}".format(pre_time.year, pre_time.month, pre_time.day,pre_time.hour,pre_time.minute,pre_time.second))
-            #print("wake up %d",wake_up_time,day[cal_day])
+            data = {'user_id' : dataCenter.user_id, 'sensor_id': dataCenter.sleep, 'num' : sleep_time, 'day': 'sunday'}
+            sleepdata = {'user_id' : dataCenter.user_id, 'sensor_id': dataCenter.sleep, 'num' : sleep_time, 'day': 'sunday'}          
+            requests.post(dataCenter.URL, json=data)
     else:
         wake_check = 0
 
