cfa7ed60e2eb713e277b26becee47418c3d0c660
이현지 <hyunjigonji@ihyeonjiui-MacBookPro.local>
2020-11-04 16:40:42 +0900

    sensor policy
    Former-commit-id: e62bbfd40b95aafed281d744e8e2477231d38bb1 [formerly 9a8bfe54b771e86c1971ff0492c231203ac46534]
    Former-commit-id: 983ea930865949f06d6d05717e606633dfb7c68f

--- robot109/image_processing/image_sleep.py
+++ robot109/image_processing/image_sleep.py
@@ -1,46 +1,52 @@
+import dataCenter
+import requests
 from datetime import datetime
+from voice_processing import voice_main
+import sensor.tail_servo_main as tail_servo_main
 
-global day, cal_day, pre_time
-day = ['monday','tuesday','wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-cal_day = datetime.today().weekday()
+import random
+
+global pre_time,nowTuple,sleep_hour, sleep_min, seconds, sleep_time
 pre_time = datetime.now()
 nowTuple = pre_time.timetuple()
-
-global sleep_hour, sleep_min, seconds, sleep_time
 sleep_hour = int(nowTuple.tm_hour)
 sleep_min = int(nowTuple.tm_min)
 seconds = (pre_time.hour * 60 + pre_time.minute) * 60 + pre_time.second
 sleep_time = seconds/3600
 
-global sleep_check, wake_check
-sleep_check = 0
-wake_check = 0
 
-def day_sleep_time():
-    global day, cal_day, pre_time
-    global sleep_hour, sleep_min, seconds, sleep_time
-    
+def day_sleep_time(sleep_check):
+    global sleep_time, sleep_hour, sleep_min
     sleep_time = seconds/3600
-    #21pm~12:30am
+    print(sleep_hour, sleep_min)
+    #75600
     if (seconds >= 75600 and seconds <= 86339) or (seconds >= 0 and seconds <= 1800) :
         if sleep_check == 0:
             sleep_check +=1
-            data = {'user_id' : '1', 'graph' : sleep_time, 'day': day[cal_day]}
+            data = {'user_id' : dataCenter.user_id, 'sensor_id': dataCenter.sleep, 'num' : sleep_time, 'day': 'sunday'}
+            requests.post(dataCenter.URL, json=data)
+            return sleep_check
     else:
         sleep_check = 0
-    #print("sleep time : {}-{}-{} {}:{}:{}".format(pre_time.year, pre_time.month, pre_time.day,pre_time.hour,pre_time.minute,pre_time.second))
-    #print("sleep %d",sleep_time,day[cal_day])
 
-def day_wake_time():
-    global day, cal_day, pre_time, sleep_hour, sleep_min, seconds,sleep_check
-    sleep_time = seconds/3600
-    #4am ~ 7:30am
+def day_wake_time(wake_check, sleepingtime):
+    global sleep_time, sleep_hour, sleep_min
+    sleep_time = seconds/60
+    sleepingtime /= 3600 
     if seconds >= 14400 and seconds<=27000:
         if wake_check == 0:
             wake_check +=1
-            data = {'user_id' : '3', 'graph' : wake_up_time, 'day': day[cal_day]}
-            #print("wake up time : {}-{}-{} {}:{}:{}".format(pre_time.year, pre_time.month, pre_time.day,pre_time.hour,pre_time.minute,pre_time.second))
-            #print("wake up %d",wake_up_time,day[cal_day])
+            # request
+            data = {'user_id' : dataCenter.user_id, 'sensor_id': dataCenter.sleep, 'num' : sleep_time, 'day': 'sunday'}
+            sleepdata = {'user_id' : dataCenter.user_id, 'sensor_id': dataCenter.sleep, 'num' : sleep_time, 'day': 'sunday'}          
+            requests.post(dataCenter.URL, json=data)
+            # NLP
+            text_list = ["일어나셨어요?","안녕히 주무셨어요?","오늘도 좋은 하루 보내세요"]
+            n = randint(0,len(text_list)-1)
+            voice_main.call_TTS(text_list[n])
+            # shake tail
+            body_servo, tail_servo = tail_servo_main.setup_tail(dataCenter.body_pin, dataCenter.tail_pin)
+            tail_servo_main.shake_tail(body_servo, tail_servo)
+            #tail_servo_main.cleanup_tail(dataCenter.body_pin, dataCenter.tail_pin)
     else:
         wake_check = 0
-
--- robot109/sensor/tail_servo_main.py
+++ robot109/sensor/tail_servo_main.py
@@ -3,8 +3,9 @@ import time
 import numpy as np
 import socket
 import sys
+import dataCenter
 
-GPIO.setmode(GPIO.BOARD)
+#GPIO.setmode(GPIO.BOARD)
 
 # variables
 global SERVO_MAX_DUTY, SERVO_MIN_DUTY
@@ -20,58 +21,41 @@ global SLEEP_START, SLEEP_END
 SLEEP_START = 0.001
 SLEEP_END = 0.01
 
-# # pins
-# body_pin = 36
-# tail_pin = 38
+def setup_tail(body_pin, tail_pin):
+    # set up pins
+    GPIO.setup(body_pin, GPIO.OUT)
+    GPIO.setup(tail_pin, GPIO.OUT)
+    body_servo = GPIO.PWM(body_pin, 50)
+    tail_servo = GPIO.PWM(tail_pin, 50)
+    body_servo.start(3)
+    tail_servo.start(7.5)
 
-# # set up pins
-# GPIO.cleanup(body_pin)
-# GPIO.cleanup(tail_pin)
-# GPIO.setup(body_pin, GPIO.OUT)
-# GPIO.setup(tail_pin, GPIO.OUT)
-# body_servo = GPIO.PWM(body_pin, 50)
-# tail_servo = GPIO.PWM(tail_pin, 50)
-# body_servo.start(3)
-# tail_servo.start(7.5)
+    return body_servo, tail_servo
 
-def main(host = "127.0.0.1", port = 4000):
-    with socket.socket() as s:
-        s.bind((host, port))
-        s.listen()
-        conn, addr = s.accept()
-        while True:
-            data = conn.recv()
-            msg = data.decode()
-            print(msg)
-            if msg == 'bye':
-                conn.close()
-                break
+def cleanup_tail(body_pin, tail_pin):
+    GPIO.cleanup(body_pin)
+    GPIO.cleanup(tail_pin)
 
-def changeTail(duty, sleepTime=DEFAULT_SLEEP):
+def changeServo(servo, duty, sleepTime=DEFAULT_SLEEP):
     #print("change tail")
-    tail_servo.ChangeDutyCycle(duty)
+    servo.ChangeDutyCycle(duty)
     time.sleep(sleepTime)
 
-def changeBody(duty, sleepTime=DEFAULT_SLEEP):
-    body_servo.ChangeDutyCycle(duty)
-    time.sleep(sleepTime)
-
-def down_tail(sleepTime=DEFAULT_SLEEP):
+def down_tail(body_servo, tail_servo, sleepTime=DEFAULT_SLEEP):
     global UP_DOWN_SLEEP
-    changeTail(7.5,sleepTime)
+    changeServo(tail_servo, 7.5, sleepTime)
     for i in np.arange(6.0,3.0,-0.05):
-        changeBody(i,sleepTime=UP_DOWN_SLEEP)
+        changeServo(body_servo, i,sleepTime=UP_DOWN_SLEEP)
 
-def up_tail(sleepTime=DEFAULT_SLEEP):
+def up_tail(body_servo, tail_servo, sleepTime=DEFAULT_SLEEP):
     global UP_DOWN_SLEEP
-    changeTail(7.5,sleepTime)
+    changeServo(tail_servo, 7.5,sleepTime)
     for i in np.arange(3.0,6.0,0.1):
-        changeBody(i,sleepTime=UP_DOWN_SLEEP)
+        changeServo(body_servo, i,sleepTime=UP_DOWN_SLEEP)
 
-def shake_tail(sleepTime=DEFAULT_SLEEP, count=DEFAULT_COUNT):
-    print("shake tail")
+def shake_tail(body_servo, tail_servo, sleepTime=DEFAULT_SLEEP, count=DEFAULT_COUNT):
     global SLEEP_START, SLEEP_END
-    up_tail(sleepTime)
+    up_tail(body_servo, tail_servo, sleepTime)
 
     # to center 
     sleepStart = SLEEP_START
@@ -79,31 +63,33 @@ def shake_tail(sleepTime=DEFAULT_SLEEP, count=DEFAULT_COUNT):
     sleepEnd = SLEEP_END
     shakeRange = np.arange(7.5,9.0,0.05)
     for idx,j in enumerate(shakeRange):
-        changeTail(j,sleepCur)
+        changeServo(tail_servo, j,sleepCur)
         sleepCur=sleepStart+((sleepEnd-sleepStart)/len(shakeRange))*idx
-    print("shake tail 2")
     # shake tail
     for i in range(count):
         sleepCur = sleepStart
         shakeRange = np.arange(9.0,6.0,-0.05)
         for idx,j in enumerate(shakeRange):
-            changeTail(j,sleepCur)
+            changeServo(tail_servo,j,sleepCur)
             sleepCur=sleepStart+((sleepEnd-sleepStart)/len(shakeRange))*idx
         shakeRange = np.arange(6.0,9.0,0.05)
         for idx,j in enumerate(shakeRange):
-            changeTail(j,sleepCur)
+            changeServo(tail_servo,j,sleepCur)
             sleepCur=sleepStart+((sleepEnd-sleepStart)/len(shakeRange))*idx
-    print("shake tail 3")
     # to center 
     sleepCur = sleepStart
     shakeRange = np.arange(9.0,7.5,-0.05)
     for idx,j in enumerate(shakeRange):
-        changeTail(j,sleepCur)
+        changeServo(tail_servo,j,sleepCur)
         sleepCur=sleepStart+((sleepEnd-sleepStart)/len(shakeRange))*idx
-    print("shake tail 4 ")
-    changeBody(6.0)
-    # GPIO.cleanup(body_pin)
-    # GPIO.cleanup(tail_pin)
+    changeServo(body_servo,6.0)
+    #GPIO.cleanup(body_pin)
+    #GPIO.cleanup(tail_pin)
+
+def main():
+    body_servo, tail_servo = setup_tail(dataCenter.body_pin, dataCenter.tail_pin)
+    shake_tail(body_servo, tail_servo)
+    cleanup_tail(dataCenter.body_pin, dataCenter.tail_pin)
 
 if __name__ == "__main__":
-    shake_tail()
+    main()
