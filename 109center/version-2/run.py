61045b2fda980fe3815d3f23fbc5348a791dd7ab
danbicyy <danbicyy@naver.com>
2020-11-06 17:21:36 +0900

    add All_user activity graph
    Former-commit-id: 40c561a6201b668b843d424c03ebf7011098fd1c [formerly e9d3448ddd3c6ba789973daf37cf1af661248af7]
    Former-commit-id: 791e1d2fd06098281ff43d5e0214a29224c6f05b

--- 109center/version-2/run.py
+++ 109center/version-2/run.py
@@ -7,7 +7,7 @@ from flask import render_template, make_response
 from flask_restful import Resource, Api
 from flask_bootstrap import Bootstrap
 from flask import  request, Response
-from flask import stream_with_context,flash
+from flask import stream_with_context,flash                                                                                                                         
 from time import sleep
 from flask_socketio import SocketIO, emit
 #from flask_mysqldb import MySQL
@@ -46,19 +46,19 @@ def sensor():
 
 @app.route('/robot_info/<data>', methods = ['POST'])
 def robot_info_post(data):
-    if request.method == 'POST': # INSERT
-        name = request.get_json().get('name')
-        robot_id = request.get_json().get('robot_id')
-        user_id = request.get_json().get('user_id')
-        print(name, robot_id, user_id)
-        dbAPI.insert_robot_info(name, robot_id, user_id)
-        return 'robot_info_post'
-    else:
-        return 'else'
+	if request.method == 'POST': # INSERT
+		name = request.get_json().get('name')
+		robot_id = request.get_json().get('robot_id')
+		user_id = request.get_json().get('user_id')
+		print(name, robot_id, user_id)
+		dbAPI.insert_robot_info(name, robot_id, user_id)
+		return 'robot_info_post'
+	else:
+		return 'else'
 
 @app.route('/user_info/<data>', methods = ['GET','POST'])
 def user_info_post(data):
-    if request.method == 'GET': # SELECT
+    if request.method == 'GET': # SELECT    
         prot_id = request.args['prot_id']
         if(data == 'id'):
             user = dbAPI.select_where("user_info",0,'id',prot_id=prot_id)
@@ -128,7 +128,7 @@ def Userapp():
     return render_template('userapp.html',row = graph_fall, data = temperature, data1 = user_info, data2 = prot_info,row1 = count_fall, data3 = humidity,wake_up = wake_up,touch_count = touch_count, sleep=sleep)
 
 
-#temperature(1), humidity(2), wake_up(3), sleep(4), fall_down(5), activity(6)
+#temperature(1), humidity(2), wake_up(3), sleep(4), fall_down(5), activity(6) 
 @app.route('/')
 def index():
     temp1 = request.args.get('user_id')
@@ -140,8 +140,9 @@ def index():
     humidity = dbAPI.select_where("sensor_data",0,"num",sensor_id = 2, user_id = 1)
     user_info = dbAPI.select_where("user_info",0,"*",id=1)
     all_user_info = dbAPI.select("user_info",0, "*")
+    user_avg_actibity = dbAPI.user_avg_activity()
     
-    return render_template('index.html',row = graph_fall, data = temperature, data1 = user_info, data2 = all_user_info ,row1 = count_fall, data3 = humidity)
+    return render_template('index.html',row = graph_fall, data = temperature, data1 = user_info, data2 = all_user_info ,row1 = count_fall, data3 = humidity,user_avg_actibity=user_avg_actibity)
 
 @app.route('/map')
 def map():
@@ -182,11 +183,12 @@ def test():
     temper_min_max = dbAPI.select_min_max(sensor_id=1,user_id=int(temp1))
     avg_wake = dbAPI.select_avg_sleep(sensor_id=3,user_id=int(temp1))
     avg_sleep = dbAPI.select_avg_sleep(sensor_id=4,user_id=int(temp1))
-    print("select avg",avg_wake)
+    ONEuser_avg_activity = dbAPI.ONEuser_avg_activity(user_id=int(temp1))
+
     
     return render_template('test.html',row = graph_fall, data = temperature, data1 = user_info, data2 = all_user_info ,
-    row1 = count_fall, data3 = humidity,wake_up = wake_up, sleep = sleep, touch_count = touch_count, prot_info = prot_info,avg_realtime = avg_realtime,
-    latest_realtime = latest_realtime, user_activity = user_activity,sub_activity = sub_activity, sub_touch = sub_touch,sub_sleeping = sub_sleeping,temper_min_max=temper_min_max,avg_wake=avg_wake,avg_sleep=avg_sleep)
+    row1 = count_fall, data3 = humidity,wake_up = wake_up, sleep = sleep, touch_count = touch_count, prot_info = prot_info,avg_realtime = avg_realtime, 
+    latest_realtime = latest_realtime, user_activity = user_activity,sub_activity = sub_activity, sub_touch = sub_touch,sub_sleeping = sub_sleeping,temper_min_max=temper_min_max,avg_wake=avg_wake,avg_sleep=avg_sleep,ONEuser_avg_activity = dbAPI.ONEuser_avg_activity(user_id=int(temp1)))
 
 
 @app.route('/contact')
@@ -224,8 +226,8 @@ def medicine_data(data):
         else: dbAPI.insert_medicine_data(name,user_id,mon,tue,wed,thu,fri,sat,sun,time1,time2,time3);
         return 'medicine_data_post';
 
-# @app.route('/ajax-trigger')
-# def ajax_trigger():
+# @app.route('/ajax-trigger') 
+# def ajax_trigger(): 
 #     return my_algorithm()
 
 @app.route('/live-data')
@@ -235,22 +237,14 @@ def live_data():
     realtime = dbAPI.select_realtime(user_id = int(temp1))
     
     data = [time() * 1000,float(realtime)]
+    print(data)
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
 
-@app.route('/activity')
-def activity_data():
-    temp1 = session['userid']
-    realtime = dbAPI.select_realtime(user_id = int(temp1))
-    
-    data = [int(realtime)]
-    response = make_response(json.dumps(data))
-    response.content_type = 'application/json'
-    return response
 
 def decodeList(input):
-    return repr(input).decode('string-escape')
+	return repr(input).decode('string-escape')
     
 @app.route('/set_rasinfo', methods = ['POST'])
 def get_rasinfo():
@@ -265,4 +259,3 @@ def get_rasinfo():
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=5000, debug=True)
-
